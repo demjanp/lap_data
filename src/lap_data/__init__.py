c050c7a5dc733fec63b58373fc9c4b61a81d4328
demjanp <peter.demjan@gmail.com>
2024-02-28 14:39:19 +0100

    update requirements to deposit 1.4.41 (fixed import and export of deposit databases)

--- src/lap_data/__init__.py
+++ src/lap_data/__init__.py
@@ -1,7 +1,7 @@
-version_info = (4, 0, 45)
+version_info = (4, 0, 46)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'Laser Aided Profiler Data Access Library'
-__date__ = "26.2.2023"
+__date__ = "28.2.2024"
 
 from lap_data.lcmodel import LCModel
