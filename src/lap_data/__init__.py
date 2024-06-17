630e2a23d9b4bd9dc2974c679f1030f5b2a52c54
demjanp <peter.demjan@gmail.com>
2024-06-17 16:45:17 +0200

    v4.1.1 updated Deposit GUI to 1.5.1; fixed querying

--- src/lap_data/__init__.py
+++ src/lap_data/__init__.py
@@ -1,7 +1,7 @@
-version_info = (4, 1, 0)
+version_info = (4, 1, 1)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'Laser Aided Profiler Data Access Library'
-__date__ = "15.6.2024"
+__date__ = "17.6.2024"
 
 from lap_data.lcmodel import LCModel
