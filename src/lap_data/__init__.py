d47b1bb26a2ad869129972e88d12c5e85880e31b
demjanp <peter.demjan@gmail.com>
2024-06-22 08:56:19 +0200

    v4.3.0;
    Modified versioning to use git tags instead of hardcoding

--- src/lap_data/__init__.py
+++ src/lap_data/__init__.py
@@ -1,7 +1,3 @@
-version_info = (4, 1, 4)
-
-__version__ = '.'.join(map(str, version_info))
-__title__ = 'Laser Aided Profiler Data Access Library'
-__date__ = "20.6.2024"
+from ._meta import title as __title__, date as __date__, __version__
 
 from lap_data.lcmodel import LCModel
---
+++ src/lap_data/_meta.py
@@ -0,0 +1,4 @@
+from ._version import version as __version__
+
+title = 'Laser Aided Profiler Data Access Library'
+date = "22.6.2024"
